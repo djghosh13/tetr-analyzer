4dcc1cf6ae4987fe23215d921ecb26c9cc91d9ac
Lauren Zhou <ren.zhou@berkeley.edu>
2021-04-10 20:39:54 -0400

    Automate drag and drop

--- browser_tools.py
+++ browser_tools.py
@@ -12,7 +12,37 @@ import json
 import sys
 import os
 
+JS_DROP_FILE = """
+    var target = arguments[0],
+        offsetX = arguments[1],
+        offsetY = arguments[2],
+        document = target.ownerDocument || document,
+        window = document.defaultView || window;
+
+    var input = document.createElement('INPUT');
+    input.type = 'file';
+    input.onchange = function () {
+      var rect = target.getBoundingClientRect(),
+          x = rect.left + (offsetX || (rect.width >> 1)),
+          y = rect.top + (offsetY || (rect.height >> 1)),
+          dataTransfer = { files: this.files };
+
+      ['dragenter', 'dragover', 'drop'].forEach(function (name) {
+        var evt = document.createEvent('MouseEvent');
+        evt.initMouseEvent(name, !0, !0, window, 0, 0, 0, x, y, !1, !1, !1, !1, 0, null);
+        evt.dataTransfer = dataTransfer;
+        target.dispatchEvent(evt);
+      });
+
+      setTimeout(function () { document.body.removeChild(input); }, 25);
+    };
+    document.body.appendChild(input);
+    return input;
+"""
+
 class Browser:
+
+    
     def __init__(self, *, speedup=2, manual_config=False, verbose=False):
         self._open = False
         self.driver = None
@@ -63,45 +93,8 @@ class Browser:
             self.wait_action("#dialogs div.oob_button.sec", "click", timeout=1 * 60)
         else:
             self._log("Setting correct configuration...")
-            for element in [
-                    "#sig_config",
-                    "h1[title='Change the way TETR.IO sounds']",
-                    "#volume_disable",
-                    "h1[title='Change the way TETR.IO functions']",
-                    "#video_actiontext_off",
-                    "#video_spin",
-                    "#video_spikes",
-                    "#video_kos",
-                    "#video_fire",
-                    "#video_siren",
-                    "h1[title='Change the way TETR.IO looks']"
-                ]:
-                self.wait_action(element, "click")
-            for element, value in [
-                    ("#video_bounciness_field", 0),
-                    ("#video_shakiness_field", 0),
-                    ("#video_gridopacity_field", 0),
-                    ("#video_boardopacity_field", 1),
-                    ("#video_shadowopacity_field", 0),
-                    ("#video_background_field", 0),
-                    ("#video_particles_field", 0.1),
-                    ("#video_pieceflash_field", 0)
-                ]:
-                try:
-                    self.wait_action(element, "click")
-                    time.sleep(0.05)
-                    self.driver.execute_script(
-                        "arguments[0].setAttribute('value', arguments[1])",
-                        self.driver.find_element_by_id("request_number"),
-                        str(value)
-                    )
-                    time.sleep(0.05)
-                    self.wait_action("#request_number_submit", "click")
-                    time.sleep(0.05)
-                except Exception as e:
-                    print(f"Errored on {element}")
-                    raise e
-            self.wait_action("#video_graphics_minimal", "click")
+
+        self.set_config()
         # Refresh and re-login
         self._log("Refreshing page to confirm settings")
         self.driver.refresh()
@@ -137,16 +130,66 @@ class Browser:
                 except Exception:
                     pass
         return success
+    def drag_and_drop_file(self, drop_target, path):
+        path = os.path.abspath(path)
+        driver = drop_target.parent
+        file_input = driver.execute_script(JS_DROP_FILE, drop_target, 0, 0)
+        file_input.send_keys(path)
+    def set_config(self):
+        target = self.driver.find_element_by_id("victoryview")
+        self.drag_and_drop_file(target, "config.ttc")
+        self.wait_action("#dialogs > div > div > div.oob_button.flex-item.sec", "click")
+        return
+        for element in [
+                "#sig_config",
+                "h1[title='Change the way TETR.IO sounds']",
+                "#volume_disable",
+                "h1[title='Change the way TETR.IO functions']",
+                "#video_actiontext_off",
+                "#video_spin",
+                "#video_spikes",
+                "#video_kos",
+                "#video_fire",
+                "#video_siren",
+                "h1[title='Change the way TETR.IO looks']"
+            ]:
+            self.wait_action(element, "click")
+        for element, value in [
+                ("#video_bounciness_field", 0),
+                ("#video_shakiness_field", 0),
+                ("#video_gridopacity_field", 0),
+                ("#video_boardopacity_field", 1),
+                ("#video_shadowopacity_field", 0),
+                ("#video_background_field", 0),
+                ("#video_particles_field", 0.1),
+                ("#video_pieceflash_field", 0)
+            ]:
+            try:
+                self.wait_action(element, "click")
+                time.sleep(0.05)
+                self.driver.execute_script(
+                    "arguments[0].setAttribute('value', arguments[1])",
+                    self.driver.find_element_by_id("request_number"),
+                    str(value)
+                )
+                time.sleep(0.05)
+                self.wait_action("#request_number_submit", "click")
+                time.sleep(0.05)
+            except Exception as e:
+                print(f"Errored on {element}")
+                raise e
+        self.wait_action("#video_graphics_minimal", "click")
 
     def open_replay(self, replayid=None):
-        if replayid is not None:
+        self.wait_action("#sig_channel", "click")
+        if not replayid.endswith(".ttrm"):
             self._log(f"Opening replay: r:{replayid}")
-            self.wait_action("#sig_channel", "click")
             self.wait_action("#tetra_find", "clear")
             self.wait_action("#tetra_find", "send_keys", "r:" + replayid)
             self.wait_action("#tetra_find", "send_keys", "\n")
         else:
-            self._log("Drag your replay file (.ttrm) into the browser...")
+            target = self.driver.find_element_by_id("victoryview")
+            self.drag_and_drop_file(target, replayid)
             WebDriverWait(self.driver, 2 * 60).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, "#multilogview:not(.hidden)"))
             )
